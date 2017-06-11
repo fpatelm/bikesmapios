d2fabbd3b106c5a61eceee46dc7f93f90d3b70c9
Faizal Patel <faizalmw@hotmail.com>
2017-06-11 14:10:52 +0100

    init projet

---
+++ BikeMapiOS/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BikeMapiOS
+//
+//  Created by Faizal Patel on 03/06/2017.
+//  Copyright © 2017 fpatel. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+use_frameworks!
+
+target 'BikeMapiOS' do
+pod 'Alamofire', '~> 4.0’
+end
+ 
+
